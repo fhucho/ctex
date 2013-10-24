2b9b605c2a88a8dec75630b100148d21320c18b1
Franta Hejl <fhucho@gmail.com>
2013-10-24 11:26:35 +0200

    Add k-means-cluster.jl

---
+++ k-means-cluster.jl
@@ -0,0 +1,22 @@
+using Clustering
+using Images
+using ImageView
+
+image = convert(Array, imread("bulb.png"))
+pixels = convert(Array{Float64,2}, transpose(reshape(image, size(image, 1) * size(image, 2), size(image, 3))))
+result = kmeans(pixels, 4)
+colors = convert(Array{Int,2}, round(result.centers))
+
+image = permutedims(image, [3, 1, 2])
+
+for i in 1:size(image, 2)
+	for j in 1:size(image, 3)
+		index =	indmin(sum((colors .- image[:,i,j]) .^ 2, 1))
+		image[:,i,j] = colors[:,index]
+	end
+end
+
+image = permutedims(image, [2, 3, 1])
+
+display(image)
+
