0a1257233c090c631c48fa8b4d9b2b2294b4cc84
Tomas Peterka <prestizni@gmail.com>
2013-12-04 12:56:23 +0100

    OpenCV progress, not much though

--- cpp/filters.cc
+++ cpp/filters.cc
@@ -1,5 +1,51 @@
 #include "filters.hh"
+#include <iostream>
+#include <array>
 
-void smooth(cv::Mat& image, int iters, int d, float alpha, float gamma) {
-    
+
+namespace ctex {
+
+    /**
+    *   This filter smooths given image and perserve medium borders.
+    *   It expects an 3 channel UINT8 image.
+    **/ 
+    void gb_fab(cv::Mat& image, int iters, int d, float alpha, float gamma) {
+        const int neighs_cnt = 4;
+
+        cv::Mat bordered(image.rows + 2, image.cols + 6, image.type());
+        cv::Point neighs[] = {cv::Point(3, 0), cv::Point(0, -1), cv::Point(-3, 0), cv::Point(0, 1)};
+
+        for(int i=0; i<iters; ++i) {
+            cv::copyMakeBorder(image, bordered, 1, 1 ,1 ,1, cv::BORDER_CONSTANT, 0);
+            
+            /** DEBUG STUFF
+            std::cout << "Original:" << std::endl << image << std::endl;
+            std::cout << "Bordered:" << std::endl << "[";
+            for(size_t x = 0; x < bordered.rows; ++x) {
+                for(size_t y = 0; y < bordered.cols * 3 ; y+=3) {
+                    std::cout << "(";
+                    for(size_t d = 0; d < 3; ++d) {
+                        std::cout << uint32_t(bordered.at<uchar>(x, y+d)) << " ";
+                    }
+                    std::cout << "), ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << "]" << std::endl;
+            **/
+            cv::Point cur = cv::Point(1, 3);
+            for(; cur.x < bordered.rows - 1; ++(cur.x)) {
+                for(; cur.y < (bordered.cols - 1) * 3 ; ++(cur.y)) {
+                    int32_t grad[neighs_cnt];
+                    for(int i=0; i < neighs_cnt; ++i) {
+                        grad[i] = bordered.at(cur) - bordered.at(cur + neighs[i])
+                    }
+                }
+            }
+        }
+
+        // divergence = sum (gradient)
+        // grad_abs * (1 + 2 * exp(-1 * (grad_abs - median) / d1(t, gamma)));
+    }
+
 }
--- cpp/main.cc
+++ cpp/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "filters.hh"
@@ -6,19 +7,26 @@
 #define FAILURE 1
 #define SUCCES 0
 
+
 int main(int argc, char *argv[]) {
 
-    cv::Mat image;
+    cv::Mat image, image_rgb, image_yiq;
 
     if (argc < 2) {
         std::cerr << "Usage: ./ctex <image_name>" << std::endl;
     }
 
-    // we suppose format CV_16U with 3 channels
+    /** 
+    *   The image is in the format CV_8U with 3 channels B G R, so the data are
+    *   in a matrix of size image.rows * (image.cols * 3). One can access these
+    *   values using image.at<uchar>(x, y). Remember y is in range (0, image.cols * 3).
+    */
     image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
     if (image.data == NULL) { return FAILURE; }
 
-    smooth(image);
+    image_rgb = image.clone();
+    ctex::gb_fab(image_rgb, 1);
+
 
     return 0;
 }
